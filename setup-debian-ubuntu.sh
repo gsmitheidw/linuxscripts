1375917ec7558e0fa0c24a37275f5af8aca9dbc2
Graham Smith <gsmitheidw@gmail.com>
2021-05-26 17:29:26 +0100

    fixes

--- setup-debian-ubuntu.sh
+++ setup-debian-ubuntu.sh
@@ -15,16 +15,16 @@ applications=$(dialog --checklist "Choose software" 10 40 3 \
         curl . on \
         mc . on \
         lshw . on \
-        moreutils . \
-        iotop . \
-        mtr . \
-        screenfetch . \
-        freeipmi . \
-        iptraf . \
-        ttyload . \
-        multitail . \
-        lsof . \
-        rsync .\
+        moreutils . off \
+        iotop . off \
+        mtr . on \
+        screenfetch . off \
+        freeipmi . off \
+        iptraf . off \
+        ttyload . off \
+        multitail . off \
+        lsof . off \
+        rsync . off \
         git . off \
         aria2c . off \
         monit . off \
@@ -34,29 +34,29 @@ applications=$(dialog --checklist "Choose software" 10 40 3 \
         autossh . off \
         sysstat . off \
         tcpdump . off \
-        smartmontools . \
+        smartmontools . off \
         httping . off \
         netcat . off  2>&1 > /dev/tty)
 
 echo "apt install $applications -y"
 
 # if screen installed:
-dpkg -s screen &> /dev/null
-if [ "$?" = "0" ];
-then
-echo GNU screen installed, downloading config to current user
-wget -O - https://gist.githubusercontent.com/gsmitheidw/6ec6eb2dce79fde80f51c7e98f17a327/raw/027c87b77841d24b3cc4421fc621a5413b51afa2/.screenrc > ~/.screenrc
-fi
+#dpkg -s screen &> /dev/null
+#if [ "$?" = "0" ];
+#then
+#echo GNU screen installed, downloading config to current user
+#wget -O - https://gist.githubusercontent.com/gsmitheidw/6ec6eb2dce79fde80f51c7e98f17a327/raw/027c87b77841d24b3cc4421fc621a5413b51afa2/.screenrc > ~/.screenrc
+#fi
 
 }
 
 
 # Setup unattended upgrades
 setup_unattended {
-apt install unattended-upgrades apt-listchanges
-dpkg-reconfigure unattended-upgrades
-vim /etc/apt/apt.conf.d/50unattended-upgrades
-unattended-upgrade -d --dry-run
+        apt install unattended-upgrades apt-listchanges
+        dpkg-reconfigure unattended-upgrades
+        vim /etc/apt/apt.conf.d/50unattended-upgrades
+        unattended-upgrade -d --dry-run
 }
 
 
