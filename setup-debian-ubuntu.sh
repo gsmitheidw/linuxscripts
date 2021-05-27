a6605b91ca7062d24cc30007ca5ce10fa5553a73
Graham Smith <gsmitheidw@gmail.com>
2021-05-27 18:33:58 +0100

    fixed cr/lf with dos2unix

--- setup-debian-ubuntu.sh
+++ setup-debian-ubuntu.sh
@@ -7,7 +7,7 @@
 # Install some apps
 install_apps {
 
-applications=$(dialog --checklist "Choose software" 10 40 3 \
+applications=$(dialog --checklist "Choose software"  20 20 30 \
         vim . on \
         screen . on \
         fail2ban . on \
@@ -38,30 +38,31 @@ applications=$(dialog --checklist "Choose software" 10 40 3 \
         httping . off \
         netcat . off  2>&1 > /dev/tty)
 
-echo "apt install $applications -y"
-
-# if screen installed:
-#dpkg -s screen &> /dev/null
-#if [ "$?" = "0" ];
-#then
-#echo GNU screen installed, downloading config to current user
-#wget -O - https://gist.githubusercontent.com/gsmitheidw/6ec6eb2dce79fde80f51c7e98f17a327/raw/027c87b77841d24b3cc4421fc621a5413b51afa2/.screenrc > ~/.screenrc
-#fi
-
+echo  apt install $applications -y
+return 0;
 }
+
+#× if screen installed:
+dpkg -s screen &> /dev/null
+if [ "$?" = "0" ];
+	then
+#	echo GNU screen installed, downloading config to current user
+#	wget -O - https://gist.githubusercontent.com/gsmitheidw/6ec6eb2dce79fde80f51c7e98f17a327/raw/027c87b77841d24b3cc4421fc621a5413b51afa2/.screenrc > ~/.screenrc
+fi
+
 
 
 # Setup unattended upgrades
-setup_unattended {
-        apt install unattended-upgrades apt-listchanges
-        dpkg-reconfigure unattended-upgrades
-        vim /etc/apt/apt.conf.d/50unattended-upgrades
-        unattended-upgrade -d --dry-run
-}
+#setup_unattended {
+#        apt install unattended-upgrades apt-listchanges
+#        dpkg-reconfigure unattended-upgrades
+#        vim /etc/apt/apt.conf.d/50unattended-upgrades
+#        unattended-upgrade -d --dry-run
+#}
 
 
 # call functions:
-install_apps
+#install_apps
 #setup_unattended
 # markdown viewer
 #pip install mdv
