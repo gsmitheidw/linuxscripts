eca4a2728f676ef8a1c2aa3e26250820565dfbc3
Graham Smith <gsmitheidw@gmail.com>
2021-07-16 12:53:56 +0100

    Removed legacy backtick lines quote around varlables to prevent globbing removed some useless use of cat

--- userscript.sh
+++ userscript.sh
@@ -17,24 +17,24 @@ nonroot="gsmith"
 cat users.txt | tr [:upper:] [:lower:] > usersL.txt; mv usersL.txt users.txt
 
 # Length of file
-len=`cat users.txt | wc -l`
+len=$(wc -l < users.txt)
 # Start counter at zero
 count=0
 
 
 # Cycle through student accounts list of X numbers:
-for username in `cat users.txt`
+for username in $(cat users.txt)
 
         do
         # Create user account
-        adduser --home /home/$username --disabled-password --gecos "" --shell /bin/bash $username
+        adduser --home /home/"$username" --disabled-password --gecos "" --shell /bin/bash "$username"
         # Generate ssh key as each user
-        su - -c "ssh-keygen -q -f ~/.ssh/id_rsa -N '' -C $username" $username
-        su - -c "cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys" $username
+        su - -c "ssh-keygen -q -f ~/.ssh/id_rsa -N '' -C $username" "$username"
+        su - -c "cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys" "$username"
         # Copy private key per user to temp directory
-        su - -c "cp ~/.ssh/id_rsa /tmp/id_rsa.$username" $username
+        su - -c "cp ~/.ssh/id_rsa /tmp/id_rsa.$username" "$username"
         # Create putty key with puttygen ~/.ssh/id_rsa -o id_rsa.ppk
-        su - -c "puttygen ~/.ssh/id_rsa -o /tmp/id_rsa.$username.ppk" $username
+        su - -c "puttygen ~/.ssh/id_rsa -o /tmp/id_rsa.$username.ppk" "$username"
         # Show Progress
         ((++count))
         echo $((count*100/len)) |  dialog --gauge "waiting" 7 50
@@ -42,13 +42,13 @@ for username in `cat users.txt`
 done
 
 # Archive the student private keys in openssh format for delivery:
-zip -r /home/$nonroot/private_keys_`date +"%d-%m-%y"`.zip /tmp/id_rsa.*
-chmod 644 /home/$nonroot/private_keys_`date +"%d-%m-%y"`.zip
+zip -r /home/$nonroot/private_keys_$(date +"%d-%m-%y").zip /tmp/id_rsa.*
+chmod 644 /home/$nonroot/private_keys_$(date +"%d-%m-%y").zip
 
 # Clean up cached private keys from temp folder for security:
 shred -u -f /tmp/id_rsa.*
 
-mv --backup=t users.txt users.`date +"%Hh-%Mm-%d-%m-%y"`.txt
+mv --backup=t users.txt users.$(date +"%Hh-%Mm-%d-%m-%y").txt
 
 dialog --msgbox "Complete! file collection from /home/$nonroot" 7 50
 clear
