ce9ade2b5feaf2510bf28f863cd523c788b8c0f2
gsmitheidw <gsmitheidw@gmail.com>
2022-03-29 15:49:12 +0100

    upgrade to elliptic curve ed25519

--- userscript.sh
+++ userscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create users, by G. Smith Feb 2014-2021
-# Requires users.txt containing list of usernames as input in the current directory
+# Requires users.txt containing list of student ids (usernames) as input in the current directory
 #
 # Depenencies: putty-tools (for puttygen) for ppk key generation, zip for packing the files for users
 #
@@ -9,49 +9,52 @@
 
 # Stop script on error
 set -e
+set -o pipefail
 
 # Select a non-root local user to collect the resulting file of ssh keys for distribution
-nonroot="gsmith"
-# specify input file here:
-input="users.txt"
+nonroot="x90021605"
 
-# convert all usernames to lowercase:
-tr '[:upper:]' '[:lower:]' < "$input" > usersL.txt
-mv usersL.txt "$input"
+# convert all student IDs to lowercase:
+cat users.txt | tr [:upper:] [:lower:] > usersL.txt; mv usersL.txt users.txt
+
+# Some random digits in case we run several times to make unique output
+rdn=`echo $RANDOM | head -c 4`
 
 # Length of file
-len=$(wc -l < users.txt)
+len=`cat users.txt | wc -l`
 # Start counter at zero
 count=0
 
 
-# Cycle through user accounts list of X numbers:
-while IFS= read -r username
+# Cycle through student accounts list of X numbers:
+for username in `cat users.txt`
 
         do
         # Create user account
-        adduser --home /home/"$username" --disabled-password --gecos "" --shell /bin/bash "$username"
+        adduser --home /home/$username --disabled-password --gecos "" --shell /bin/bash $username
         # Generate ssh key as each user
-        su - -c "ssh-keygen -q -f ~/.ssh/id_rsa -N '' -C $username" "$username"
-        su - -c "cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys" "$username"
+        su - -c "ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519 -N '' -C $username" $username
+        su - -c "cp ~/.ssh/id_ed25519.pub ~/.ssh/authorized_keys" $username
         # Copy private key per user to temp directory
-        su - -c "cp ~/.ssh/id_rsa /tmp/id_rsa.$username" "$username"
+        su - -c "cp ~/.ssh/id_ed25519 /tmp/id_ed25519.$username" $username
         # Create putty key with puttygen ~/.ssh/id_rsa -o id_rsa.ppk
-        su - -c "puttygen ~/.ssh/id_rsa -o /tmp/id_rsa.$username.ppk" "$username"
+        su - -c "puttygen ~/.ssh/id_ed25519 -o /tmp/id_ed25519.$username.ppk" $username
         # Show Progress
         ((++count))
         echo $((count*100/len)) |  dialog --gauge "waiting" 7 50
 
-done < "$input"
+done
 
-# Archive the users private keys in openssh format for delivery:
-zip -r /home/$nonroot/private_keys_$(date +"%d-%m-%y").zip /tmp/id_rsa.*
-chmod 644 /home/$nonroot/private_keys_$(date +"%d-%m-%y").zip
+
+# Archive the student private keys in openssh format for delivery:
+zip -r /home/$nonroot/"$HOSTNAME"_private_keys_`date +"%d-%m-%y-$rdn"`.zip /tmp/id_ed25519.*
+chmod 644 /home/$nonroot/"$HOSTNAME"_private_keys_`date +"%d-%m-%y-$rdn"`.zip
 
 # Clean up cached private keys from temp folder for security:
-shred -u -f /tmp/id_rsa.*
 
-mv --backup=t users.txt users.$(date +"%Hh-%Mm-%d-%m-%y").txt
+shred -u -f /tmp/id_ed25519.*
+# backup
+mv --backup=t users.txt users.`date +"%d-%m-%y_at_%Hh-%Mm-$rdn"`.log
 
-dialog --msgbox "Complete! file collection from /home/$nonroot" 7 50
+dialog --msgbox "Complete! file collection "$HOSTNAME"_private_keys_`date +"%d-%m-%y-$rdn"`.zip from /home/$nonroot" 7 50
 clear
