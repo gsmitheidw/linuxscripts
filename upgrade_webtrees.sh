16e9207ffa62418be485242b4e34091fabf37d34
gsmitheidw <gsmitheidw@gmail.com>
2022-08-03 16:52:32 +0100

    check disk space

--- upgrade_webtrees.sh
+++ upgrade_webtrees.sh
@@ -1,10 +1,19 @@
 #/bin/bash
 # Webtrees Manual Upgrade (To Latest)
 
+reqspace='200'
+freespace=$(df -m "/" | awk 'END{print $4}') 
+if [[ freespace -le reqspace ]]
+then
+	echo "Not enough space to proceed"
+	exit 1
+fi
+
+
 webtrees_root='/var/www/html'
 cd $webtrees_root
-
 touch webtrees/data/offline.txt
+
 
 # Obtain the url for the latest release
 url=$(curl -s https://api.github.com/repos/fisharebest/webtrees/releases/latest | grep browser_download_url | cut -d '"' -f 4)
