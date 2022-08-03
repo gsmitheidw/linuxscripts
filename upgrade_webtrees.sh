aefa12f0412293c79bc348a24d1a612713916fc7
gsmitheidw <gsmitheidw@gmail.com>
2022-08-03 15:31:11 +0100

    create upgrade script

---
+++ upgrade_webtrees.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+# Webtrees Manual Upgrade (To Latest)
+
+webtrees_root='/var/www/html'
+cd $webtrees_root
+
+touch webtrees/data/offline.txt
+
+# Obtain the url for the latest release
+url=$(curl -s https://api.github.com/repos/fisharebest/webtrees/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+wget $url
+
+filemname=$(echo $url | cut -b 65-82)
+
+unzip -o $filename
+
+rm webtrees/data/offline.txt 
