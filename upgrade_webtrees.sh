5c41137a109da560fa599862de546623f4f01ab6
gsmitheidw <gsmitheidw@gmail.com>
2022-08-03 15:38:10 +0100

    typo

--- upgrade_webtrees.sh
+++ upgrade_webtrees.sh
@@ -10,7 +10,7 @@ touch webtrees/data/offline.txt
 url=$(curl -s https://api.github.com/repos/fisharebest/webtrees/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 wget $url
 
-filemname=$(echo $url | cut -b 65-82)
+filename=$(echo $url | cut -b 65-82)
 
 unzip -o $filename
 
