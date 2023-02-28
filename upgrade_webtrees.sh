cd8a8b39ca0f646304bb23e39f7048eb9ea3814f
gsmitheidw <gsmitheidw@gmail.com>
2023-02-28 11:00:17 +0000

    various fixes

--- upgrade_webtrees.sh
+++ upgrade_webtrees.sh
@@ -1,26 +1,32 @@
 #/bin/bash
 # Webtrees Manual Upgrade (To Latest)
 
+type curl >/dev/null 2>&1 || { echo >&2 "Require curl but it's not installed.  Aborting."; exit 1; }
+type wget >/dev/null 2>&1 || { echo >&2 "Require wget but it's not installed.  Aborting."; exit 1; }
+
 reqspace='200'
 freespace=$(df -m "/" | awk 'END{print $4}') 
 if [[ freespace -le reqspace ]]
 then
-	echo "Not enough space to proceed"
-	exit 1
+		echo "Not enough space to proceed"
+		exit 1
 fi
 
-
-webtrees_root='/var/www/html'
+echo "Starting upgrade in 5 seconds..."
+sleep 5
+webtrees_root="/var/www/html"
 cd $webtrees_root
-touch webtrees/data/offline.txt
+touch $webtrees_root/webtrees/data/offline.txt
 
 
 # Obtain the url for the latest release
 url=$(curl -s https://api.github.com/repos/fisharebest/webtrees/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 wget $url
 
-filename=$(echo $url | cut -b 65-82)
+filename=$(basename "$url")
 
 unzip -o $filename
 
-rm webtrees/data/offline.txt 
+rm $webtrees_root/webtrees/data/offline.txt 
+
+
